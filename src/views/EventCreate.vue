cc4e627ac1d889212b4c65f57de279f579484480
Selene Arzola <selene.arzola@gmail.com>
2022-02-22 16:51:07 -0600

    a date picker library was implemented

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -23,18 +23,19 @@
 
 			<h3>When is your event?</h3>
 			<label>Date</label>
-			<input v-model="event.date" type="text" placeholder="Date" />
-
+			<datepicker v-model="event.date" :inputFormat="'LLLL d yyyy'">
+			</datepicker>
 			<label>Time</label>
 			<input v-model="event.time" type="text" placeholder="Time" />
-
 			<button type="submit">Submit</button>
 		</form>
 	</div>
 </template>
 <script>
 	import { v4 as uuidv4 } from 'uuid'
+	import Datepicker from 'vue3-datepicker'
 	export default {
+		components: { Datepicker },
 		data() {
 			return {
 				categories: [
@@ -46,13 +47,27 @@
 					'food',
 					'community'
 				],
+				months: [
+					'January',
+					'February',
+					'March',
+					'April',
+					'May',
+					'June',
+					'July',
+					'August',
+					'September',
+					'October',
+					'November',
+					'December'
+				],
 				event: {
 					id: '',
 					category: '',
 					title: '',
 					description: '',
 					location: '',
-					date: '',
+					date: new Date(),
 					time: '',
 					organizer: '',
 				}
@@ -60,9 +75,10 @@
 		},
 		methods: {
 			onSubmit() {
-
 				this.event.id = uuidv4()
 				this.event.organizer = this.$store.state.user
+				const d = this.event.date
+				this.event.date = `${this.months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
 				this.$store
 					.dispatch('createEvent', this.event)
 					.then(() => {
--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,11 +1,10 @@
 <template>
 	<div v-if="event">
 		<h1>{{ event.title }}</h1>
-		<p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+		<p>{{ event.time }} on {{ formattedDate }} @ {{ event.location }}</p>
 		<p>{{ event.description }}</p>
 	</div>
 </template>
-
 <script>
 	export default {
 		props: ['id'],
